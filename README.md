58d680c6c041d1947744cfbc018e9a3838c701c8
Victor Csiky <strongholdmedia@users.noreply.github.com>
2020-06-24 13:58:38 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# usable.js
+# usable.js v0.1.0
 
 ## Making the Web / DOM usable
 
@@ -18,6 +18,8 @@ then
 
 `require("@dorgaren/usable").install();`
 
+**Please note that for this to work, you _must absolutely_ load it _before_ anything else, _including_ lodash/underscore, jQuery, React, and Angular.**
+
 `install()` accepts an optional argument for the global object.
 If you run it in Node but have a `window` global for anything (else), or conversely, are using it in browser but having a `global` global reserved for something, you may hint at which global object to use, by choosing
 
@@ -28,6 +30,25 @@ or
 `require("@dorgaren/usable").install(global); // Node`
 
 explicitly.
+
+**Not using Webpack/NPM?**
+
+In this case, I recommend that you use require.js.
+Download require.js and put it alongside your "site" script (here, `site.js`).
+
+Replace the loading of your site script in the head with
+
+```
+    <script data-main="script/site" src="script/require.js"></script>
+```
+
+Then begin your "site" script with
+
+```
+requirejs([ "@dorgaren/usable" ], function(usable) { usable.install(); });
+```
+This way, anything you do with Usable will remain compatible when/if (ever) you
+switch to using a bundler.
 
 ### Current aims
 
@@ -51,23 +72,44 @@ _In general_, however, apart from some rants, this project mainly tries to take 
 
 ### Caveats
 
+* Performance
+
 **Please note that some implementations here may not be the most optimal approaches, either in general, or on the specific platform you use.**
 
 This will definitely evolve over time, as long as the underlying proof-of-concept is viable.
 
+* No support for "options" in "addEventListener"
+
+Will follow soon (need to check the proposed inconsistencies across platforms).
+
 ### FAQ
 
-1. Why is this not in pure ES5 / ES6 / whatever syntax?
+**1. Why is this not in pure ES5 / ES6 / whatever syntax?**
 
 Most of the stuff that needs to be accessible for this kind of matter to work is either _inaccessible_ or _discouraged_ in ES6.
-Meanwhile, there are a lot of "syntactic sugars" that are generally useful in ES6, causing little to no ambiguity, and are supported 
+Meanwhile, there are a lot of "syntactic sugars" that are generally useful in ES6, causing little to no ambiguity, and are supported
 
 This should work _natively_ in IE11 and anything from 2016 onwards.
 
 _Should you have any issues, though, just run this through some "transpiler"._ I tested it extensively with `Buble`.
 
-2. Why no minification / packaging?
+**2. Why slow / no minification / packaging etc?**
 
 Please consider all of this _experimental_ before 1.0.
 That means that _the core goals are warranted_, but nothing else, neither expressed nor implied.
 This latter includes things like payload size or computational performance.
+
+*Note that the notion _do not expect_ does _not at all_ mean that they _won't happen_ any sooner.*
+Just do not fill the issues queue with stuff like that for the time being, please.
+
+But please **do** open issues for anything _extraordinary_, i.e. if you find a platform that is like 10-100x slower using this than the others you tested.
+
+**3. Why CommonJS???**
+
+While this is supposed to work great in Node.js, its primary target is _the browser_.
+
+Then again, to use such functionality, one _needs_ ES5 syntax, that is, use object mangling, and the prototype approach.
+
+_In my opinion (yes, this is personal)_, ES module syntax looks very odd with prototype mangling.
+
+Also, backwards compatibility is an internal requirement.
---
+++ src/usable.js
@@ -0,0 +1,301 @@
+/**
+ * Usable.js, version 0.1.0
+ *
+ * Copyright (c) 2020, Victor Csiky, Stronghold-Terra Nonprofit llc
+ * All rights reserved.
+ *
+ * See LICENSE (BSD 3-clause) for details.
+ */
+const Usable = {
+    install: function install(globalObject)
+    {
+        globalObject = globalObject || window || global;
+
+        // this shim is based on Polymer Project's work; thanks!
+        if (typeof WeakMap === "undefined")
+        {
+            let WeakMap = function WeakMap()
+            {
+                this.name = "_$$" + (Math.random() * 1e9 >>> 0) + (Date.now() % 1e9 + 1) + "$$";
+            };
+
+            WeakMap.prototype = {
+                set: function setEntry(key, value)
+                {
+                    let entry = key[this.name];
+
+                    if (entry && entry[0] === key)
+                    {
+                        entry[1] = value;
+                    }
+                    else
+                    {
+                        Object.defineProperty(key, this.name, { value: [ key, value ], writable: true });
+                    }
+
+                    return this;
+                },
+                get: function getEntry(key)
+                {
+                    let entry = key[this.name];
+
+                    return entry && entry[0] === key
+                        ? entry[1]
+                        : undefined;
+                },
+                delete: function deleteEntry(key)
+                {
+                    let entry = key[this.name],
+                        retVal = entry && entry[0] === key;
+
+                    if (retVal) { entry[0] = entry[1] = undefined; }
+
+                    return retVal;
+                },
+                has: function hasEntry(key)
+                {
+                    let entry = key[this.name];
+
+                    return entry && entry[0] === key;
+                }
+            };
+
+            globalObject.WeakMap = WeakMap;
+        }
+
+        let eventHandlerQueue = new WeakMap,
+            eventNameCache = {},
+            oldEventTargetAddEventListener = globalObject.EventTarget.prototype.addEventListener,
+            oldEventTargetRemoveEventListener = globalObject.EventTarget.prototype.removeEventListener,
+            oldNodeAppendChild = null,
+            oldParentNodeAppend = null;
+
+        globalObject.EventTarget.prototype.addEventHandler = function addEventHandler(name, handler, options)
+        {
+            let targetQueue = eventHandlerQueue.get(this) || {},
+                key = "E" + ((Math.random() * 1e9 >>> 0) - (Date.now() % 1e9 + 1));
+
+            while (typeof eventNameCache[key] !== "undefined")
+            {
+                key = "E" + ((Math.random() * 1e9 >>> 0) - (Date.now() % 1e9 + 1));
+            }
+
+            eventNameCache[key] = name;
+            targetQueue[key] = {
+                handler: handler,
+                options: options
+            };
+
+            eventHandlerQueue.set(this, targetQueue);
+
+            oldEventTargetAddEventListener.call(this, name, handler, options);
+
+            return key;
+        };
+
+        globalObject.EventTarget.prototype.addEventListener = function addEventListener(name, handler, options)
+        {
+            this.addEventHandler(name, handler, options);
+        };
+
+        globalObject.EventTarget.prototype.removeEventHandler = function removeEventHander(key)
+        {
+            if (typeof eventNameCache[key] === "undefined")
+            {
+                throw new ReferenceError("Key not found in event name cache");
+            }
+
+            let targetQueue = eventHandlerQueue.get(this) || {};
+
+            oldEventTargetRemoveEventListener.call(
+                this,
+                eventNameCache[key],
+                targetQueue[key].handler,
+                targetQueue[key].options
+            );
+
+            delete targetQueue[key];
+            delete eventNameCache[key];
+        };
+
+        globalObject.EventTarget.prototype.removeEventListener = function removeEventListener(name, handler, options)
+        {
+            let targetQueue = eventHandlerQueue.get(this),
+                match = null;
+
+            if (typeof targetQueue !== "object")
+            {
+                throw new ReferenceError("No event handlers registered on this target");
+            }
+
+            let keys = Object.keys(targetQueue);
+
+            for (let idx = 0; idx < keys.length; idx++)
+            {
+                if (eventNameCache[keys[idx]] === name)
+                {
+                    let candidate = targetQueue[keys[idx]];
+                    if (candidate.handler === handler && candidate.options === options)
+                    {
+                        match = keys[idx];
+                        break;
+                    }
+                }
+            }
+
+            if (match === null)
+            {
+                throw new Error("Cannot remove event handler - no match found");
+            }
+
+            this.removeEventHandler(match);
+        };
+
+        globalObject.EventTarget.prototype.hasEventListener = function hasEventListener(name, handler, options)
+        {
+            if (typeof name !== "string" && handler)
+            {
+                throw new TypeError("Event type must not be null when handler reference provided");
+            }
+
+            let targetQueue = eventHandlerQueue.get(this),
+                retVal = false;
+
+            if (typeof targetQueue === "object")
+            {
+                let keys = Object.keys(targetQueue);
+
+                if (keys.length > 0)
+                {
+                    for (let idx = 0; idx < keys.length; idx++)
+                    {
+                        if (eventNameCache[keys[idx]] === name
+                            && (!handler
+                                || (handler === targetQueue[keys[idx]].handler
+                                    && options === targetQueue[keys[idx]].options
+                                )
+                            )
+                        )
+                        {
+                            retVal = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return retVal;
+        };
+
+        globalObject.EventTarget.prototype.getEventHandlers = function getEventHandlers(name)
+        {
+            let targetQueue = eventHandlerQueue.get(this),
+                retVal = {};
+
+            if (typeof targetQueue === "object")
+            {
+                let keys = Object.keys(targetQueue);
+
+                for (let idx = 0; idx < keys.length; idx++)
+                {
+                    retVal[eventNameCache[keys[idx]]] = retVal[eventNameCache[keys[idx]]] || [];
+                    retVal[eventNameCache[keys[idx]]].push(Object.assign(
+                        {
+                            key: keys[idx],
+                            handler: targetQueue[keys[idx]].handler
+                        },
+                        typeof targetQueue[keys[idx]].options === "object"
+                            ? { options: targetQueue[keys[idx]].options }
+                            : { useCapture: targetQueue[keys[idx]].options }
+                    ));
+                }
+            }
+
+            return retVal;
+        };
+
+        //
+        if (typeof globalObject.DocumentFragment === "function")
+        {
+            oldNodeAppendChild = globalObject.Node.prototype.appendChild;
+            oldParentNodeAppend = typeof globalObject.ParentNode === "function"
+                ? globalObject.ParentNode.prototype.append
+                : null;
+
+            globalObject.EventTarget.prototype.migrateEventHandlersTo = function migrateEventHandlersTo(newTarget, detachCurrent)
+            {
+                let retVal = false;
+
+                if (! newTarget instanceof EventTarget)
+                {
+                    throw new TypeError("'newTarget' must be an EventTarget instance");
+                }
+
+                let targetQueue = eventHandlerQueue.get(this);
+
+                if (typeof targetQueue === "object")
+                {
+                    let keys = Object.keys(targetQueue);
+
+                    for (let idx = 0; idx < keys.length; idx++)
+                    {
+                        let key = keys[idx];
+
+                        if (typeof eventNameCache[key] === "undefined")
+                        {
+                            throw new InternalError("State corruption");
+                        }
+
+                        newTarget.addEventHandler(
+                            eventNameCache[key],
+                            targetQueue[key].handler,
+                            targetQueue[key].options
+                        );
+
+                        if (detachCurrent) { this.removeEventHandler(key); }
+                    }
+
+                    retVal = true; // perhaps enhance this later
+                }
+
+                return retVal;
+            };
+
+            globalObject.Node.prototype.appendChild = function appendChild(child)
+            {
+                let retVal = oldNodeAppendChild.call(this, child);
+
+                if (child instanceof DocumentFragment)
+                {
+                    child.migrateEventHandlersTo(this);
+                }
+
+                return retVal;
+            };
+
+            if (oldParentNodeAppend !== null)
+            {
+                globalObject.ParentNode.prototype.append = function append()
+                {
+                    let args = Array.from(arguments),
+                        eventHandlerSources = [];
+
+                    for (let idx = 0; idx < args.length; idx++)
+                    {
+                        // perhaps passing the whole argument set is faster,
+                        // but unsure if any current or future events will not
+                        // interfere with event handler order
+                        oldParentNodeAppend.call(this, args[idx]);
+
+                        if (args[idx] instanceof DocumentFragment)
+                        {
+                            args[idx].migrateEventHandlersTo(this);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+module.exports = Usable;
